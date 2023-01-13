e4acf0773d63d338b04c71cf3c888d1de9015823
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-12 16:30:32 -0800

    grabbed DOM elements

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 
 /* Get DOM Elements */
 
+const climateDropdown = document.getElementById('climate-dropdown');
+const architectureDropdown = document.getElementById('architecture-dropdown');
+const attractionsDropdown = document.getElementById('attractions-dropdown');
+const climateCountEl = document.getElementById('climate-count');
+const architectureCountEl = document.getElementById('architecture-count');
+const attractionsCountEl = document.getElementById('attractions-count');
+const climateImage = document.getElementById('climate-image');
+const architectureImage = document.getElementById('architecture-image');
+const attractionsImage = document.getElementById('attractions-image');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+const slogansEl = document.getElementById('slogans');
+
 /* State */
 
 /* Events */
